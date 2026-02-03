e91c9353602981d38f0666426d005bdb5487e747
ahvha7600-beep <ahvha7600@gmail.com>
2026-02-03 22:09:13 +0300

    Initialize plugin package and import components

---
+++ plugin/__init__.py
@@ -0,0 +1,3 @@
+# plugin package init
+from .game_activity import GameActivityManager, ConsoleAdapter, TelegramClientAdapter
+__all__ = ["GameActivityManager", "ConsoleAdapter", "TelegramClientAdapter"]
